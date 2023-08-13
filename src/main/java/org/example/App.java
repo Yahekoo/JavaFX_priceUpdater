8320e0e1c24fca8eb8556b3141fa1a5f0cd2a0d1
Yahekoo <zafin.dev@gmail.com>
2023-08-13 16:40:27 +0100

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
